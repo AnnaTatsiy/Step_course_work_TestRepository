3b8b2c333a8399319f89daad42d76a7388a36c42
Anna <annatatsiy2342@gmail.com>
2022-08-05 21:45:31 +0300

    Первый коммит

---
+++ Step_course_work1(Anna_Tatsiy)/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_course_work1_Anna_Tatsiy_.Models
+{
+    internal class Person// класс для сущности персоны 
+    {
+        public int Id { get; set; } //Id
+        public string Name { get; set; }//Имя
+        public string Surename { get; set; }//Фамилия
+        public string Patronymic { get; set; }//Отчество 
+    }
+}
